5628601609ed9702ed6f1fd589785d4ce615fccb
Rom Srinivasan <Rom Srinivasan@localhost>
2013-02-01 13:00:14 -0800

    ServerLoader class to load test Martus server

--- source/org/martus/loadtest/ServerLoader.java
+++ source/org/martus/loadtest/ServerLoader.java
@@ -1,15 +1,25 @@
 package org.martus.loadtest;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.martus.client.bulletinstore.ClientBulletinStore;
 import org.martus.clientside.ClientSideNetworkGateway;
 import org.martus.clientside.ClientSideNetworkHandlerUsingXmlRpcForNonSSL;
+import org.martus.common.MartusLogger;
+import org.martus.common.MartusUtilities;
+import org.martus.common.bulletin.Bulletin;
+import org.martus.common.bulletin.BulletinZipUtilities;
 import org.martus.common.crypto.MartusCrypto;
 import org.martus.common.crypto.MartusSecurity;
 import org.martus.common.network.NetworkInterfaceConstants;
 import org.martus.common.network.NetworkResponse;
 import org.martus.common.network.NonSSLNetworkAPIWithHelpers;
+import org.martus.common.packet.UniversalId;
+import org.martus.util.DirectoryUtils;
 import org.martus.util.StreamableBase64;
 
 /**
@@ -18,28 +28,27 @@ import org.martus.util.StreamableBase64;
  */
 public class ServerLoader {
 
-    public ServerLoader(String serverPublicKey, String serverIP, String magicWord, String storeDir)
+    public ServerLoader(String serverIP, String magicWord, int numThreads, int numBulletins)
     {
-        this.serverPublicKey = serverPublicKey;
         this.serverIP = serverIP;
         this.magicWord = magicWord;
-        this.storeDir = storeDir;
+        this.numThreads = numThreads;
+        this.numBulletins = numBulletins;
     }
 
     public final static void main( String[] args )
     {
-        if( args.length < 5 )
+        if( args.length < 4 )
         {
             usage("Not enough arguments.");
         }
         else {
-            String storeDir = args[0];
-            String serverIp = args[1];
-            String serverPublicKey = args[2];
-            String magicWord = args[3];
-            int numThreads = Integer.valueOf(args[4]);
+            String serverIp = args[0];
+            String magicWord = args[1];
+            int numThreads = Integer.valueOf(args[2]);
+            int numBulletins = Integer.valueOf(args[3]);
 
-            final ServerLoader loader = new ServerLoader(serverPublicKey, serverIp, magicWord, storeDir);
+            final ServerLoader loader = new ServerLoader(serverIp, magicWord, numThreads, numBulletins);
             loader.startLoading();
 
         }
@@ -48,52 +57,117 @@ public class ServerLoader {
     public void startLoading()
     {
 
-        try {
+        try
+        {
             martusCrypto = new MartusSecurity();
             martusCrypto.createKeyPair();
 
             NonSSLNetworkAPIWithHelpers server = new ClientSideNetworkHandlerUsingXmlRpcForNonSSL(serverIP);
             String result = server.getServerPublicKey(martusCrypto);
-            if (confirmServerPublicKey(serverPublicKey, result))
-            {
-                System.out.println("Server info okay");
-            } else
-            {
-                System.out.println("Server info not correct");
-                return;
-            }
 
-            gateway = ClientSideNetworkGateway.buildGateway(serverIP, serverPublicKey);
+            gateway = ClientSideNetworkGateway.buildGateway(serverIP, result);
             NetworkResponse response = gateway.getUploadRights(martusCrypto, magicWord);
             if (!response.getResultCode().equals(NetworkInterfaceConstants.OK))
             {
-                System.out.println("couldn't verify magic word");
+                MartusLogger.log("couldn't verify magic word");
                 return;
             }
 
             //can now create bulletins;
         } catch (Exception e) {
-            System.err.println("unable to verify server info");
-            e.printStackTrace();
+            MartusLogger.log("unable to verify server info");
+            MartusLogger.logException(e);
         }
 
         store = new ClientBulletinStore(martusCrypto);
         try
         {
-            store.doAfterSigninInitialization(new File(storeDir));
+            tempDir = DirectoryUtils.createTempDir();
+            store.doAfterSigninInitialization(tempDir);
+            store.createFieldSpecCacheFromDatabase();
         } catch (Exception e) {
-            System.err.println("unable to initialize store");
-            e.printStackTrace();
+            MartusLogger.log("unable to initialize store");
+            MartusLogger.logException(e);
+        }
+
+        zippedBulletins = new File[numBulletins];
+        bulletinIds = new UniversalId[numBulletins];
+        try
+        {
+            createZippedBulletins();
+
+            ExecutorService executor = Executors.newFixedThreadPool(numThreads);
+            int i = 0;
+            for (File file : zippedBulletins) {
+                Runnable sender = new BulletinSenderRunnable(martusCrypto, gateway, file, bulletinIds[i++]);
+                executor.execute(sender);
+            }
+            executor.shutdown();
+            while (!executor.isTerminated()) {
+                //do nothing - just waiting
+            }
+            MartusLogger.log("Finished sending bulletins");
+        } catch (Exception e) {
+            MartusLogger.log("problem sending bulletins");
+            MartusLogger.logException(e);
         }
 
     }
 
-    private boolean confirmServerPublicKey(String serverCode, String serverPublicKey) throws StreamableBase64.InvalidBase64Exception
+    private void createZippedBulletins() throws Exception
     {
-        final String normalizedPublicCode = MartusCrypto.removeNonDigits(serverCode);
-        final String computedCode;
-        computedCode = MartusCrypto.computePublicCode(serverPublicKey);
-        return normalizedPublicCode.equals(computedCode);
+        for (int i = 0; i < numBulletins; i++) {
+            Bulletin bulletin = createBulletin(i);
+            store.saveBulletin(bulletin);
+            File file = File.createTempFile("tmp_send_", ".zip", tempDir);
+            BulletinZipUtilities.exportBulletinPacketsFromDatabaseToZipFile(store.getDatabase(),
+                    bulletin.getDatabaseKey(), file, bulletin.getSignatureGenerator());
+            zippedBulletins[i] = file;
+            bulletinIds[i] = bulletin.getUniversalId();
+            store.destroyBulletin(bulletin);
+        }
+    }
+
+    private Bulletin createBulletin(int num) throws Exception
+    {
+        Bulletin b = store.createEmptyBulletin();
+        b.set(Bulletin.TAGTITLE, "loadtest title " + num);
+        b.set(Bulletin.TAGSUMMARY, "loadtest summary " + num);
+        b.setDraft();
+        b.setAllPrivate(true);
+        return b;
+    }
+
+    public static String uploadBulletinZipFile(UniversalId uid, File tempFile, ClientSideNetworkGateway gateway,
+            MartusCrypto crypto)
+            throws MartusUtilities.FileTooLargeException, IOException, MartusCrypto.MartusSignatureException
+    {
+        final int totalSize = MartusUtilities.getCappedFileLength(tempFile);
+        int offset = 0;
+        byte[] rawBytes = new byte[NetworkInterfaceConstants.CLIENT_MAX_CHUNK_SIZE];
+        FileInputStream inputStream = new FileInputStream(tempFile);
+        String result = null;
+        while(true)
+        {
+            int chunkSize = inputStream.read(rawBytes);
+            if(chunkSize <= 0)
+                break;
+            byte[] chunkBytes = new byte[chunkSize];
+            System.arraycopy(rawBytes, 0, chunkBytes, 0, chunkSize);
+
+            String authorId = uid.getAccountId();
+            String bulletinLocalId = uid.getLocalId();
+            String encoded = StreamableBase64.encode(chunkBytes);
+
+            NetworkResponse response = gateway.putBulletinChunk(crypto,
+                                authorId, bulletinLocalId, totalSize, offset, chunkSize, encoded);
+            result = response.getResultCode();
+            if(!result.equals(NetworkInterfaceConstants.CHUNK_OK) && !result.equals(NetworkInterfaceConstants.OK))
+                break;
+            offset += chunkSize;
+        }
+        inputStream.close();
+        return result;
     }
 
 
@@ -105,15 +179,18 @@ public class ServerLoader {
     private static void usage( String msg )
     {
         System.err.println( msg );
-        System.err.println( "Usage: java ServerLoader  /tmp 54.245.101.104 8714.7632.8884.7614.8217 spam 10" );
+        System.err.println( "Usage: java ServerLoader  <server ip> <magic word>  <number of threads> <number of bulletins>" );
     }
 
 
     private MartusSecurity martusCrypto;
-    private ClientSideNetworkGateway gateway = null;
+    private ClientSideNetworkGateway gateway;
     private String serverIP;
-    private String serverPublicKey;
     private String magicWord;
     private ClientBulletinStore store;
-    private String storeDir;
+    private File tempDir;
+    private int numThreads;
+    private int numBulletins;
+    File[] zippedBulletins;
+    UniversalId[] bulletinIds;
 }
